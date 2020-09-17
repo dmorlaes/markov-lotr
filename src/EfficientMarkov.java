9ec8fcbef0da1406edbe596a51fbdb1890be3c90
Dmitri Morales <dmitri.morales@duke.edu>
2020-09-16 22:12:36 -0400

    I finished efficientMarkov

--- src/EfficientMarkov.java
+++ src/EfficientMarkov.java
@@ -11,4 +11,27 @@ public class EfficientMarkov extends BaseMarkov {
 		super(order);
 		myMap = new HashMap<>();
 	}
-}	
+	public void setTraining(String text) {
+		super.setTraining(text);
+		myMap.clear();
+		int index = 0;
+		while (index + myOrder < myText.length()) {
+			String s = myText.substring(index, index + myOrder);
+			ArrayList<String> list = new ArrayList<>();
+			myMap.putIfAbsent(s, list);
+			myMap.get(s).add(myText.substring(index + myOrder, index + myOrder + 1));
+			index++;
+
+		}
+	}
+		@Override
+	public ArrayList<String> getFollows(String key) {
+			if(! (myMap.containsKey(key))) {
+				throw new NoSuchElementException(key + " not in map");
+			}
+			return myMap.get(key);
+
+
+		}
+	}
+
