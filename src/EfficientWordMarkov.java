d5416ccc021e3ff3482d8601b28c7bb2a5e9793d
Dmitri Morales <dmitri.morales@duke.edu>
2020-09-18 20:12:26 -0400

    I finished

---
+++ src/EfficientWordMarkov.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+public class EfficientWordMarkov extends BaseWordMarkov{
+    private Map<WordGram, ArrayList<String>> myMap;
+
+    EfficientWordMarkov(){
+        this(3);
+    }
+
+    EfficientWordMarkov(int order) {
+        super(order);
+        myMap = new HashMap<>();
+    }
+    public void setTraining(String text) {
+        super.setTraining(text);
+        myMap.clear();
+        int index = 0;
+        myWords = text.split("\\s+");
+        while (index + myOrder < myWords.length) {
+            WordGram nGram = new WordGram(myWords, index, myOrder);
+            ArrayList<String> mapList = new ArrayList<>();
+            myMap.putIfAbsent(nGram, mapList);
+            myMap.get(nGram).add(myWords[index + myOrder]);
+            index++;
+
+        }
+    }
+    @Override
+    public ArrayList<String> getFollows(WordGram oGram) {
+        if(! (myMap.containsKey(oGram))) {
+            throw new NoSuchElementException(oGram.toString() + " not in map");
+        }
+        return myMap.get(oGram);
+
+
+    }
+}
